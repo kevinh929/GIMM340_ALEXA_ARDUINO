c5c3f1b4127552a9fa5c4909df5a19b28c1223d5
Gavin Tafoya <83233027+GavinTafoya@users.noreply.github.com>
2025-12-09 17:02:29 -0700

    server code was goofy

--- index.js
+++ index.js
@@ -17,50 +17,45 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/kid/', async (req, res) => {
     let result = {};
-    async (request, response) => {
-        try {
-            console.log("Query Parameters: ", req.query);
-            result = await kids.getAll(req.query);
-            console.log("Kids fetched: ", result);
-        }
-        catch (error) {
-            console.error("Error fetching kids: ", error);
-            response.status(500).send("Error fetching kids");
-        }
-        response.json({'data': result });
+    
+    try {
+        console.log("Query Parameters: ", req.query);
+        result = await kids.getAll(req.query);
+        console.log("Kids fetched: ", result);
     }
+    catch (error) {
+        console.error("Error fetching kids: ", error);
+        res.status(500).send("Error fetching kids");
+    }
+    res.json({'data': result });
 });
 
 app.get('/bedtime/', async (req, res) => {
     let result = {};
-    async (request, response) => {
-        try {
-            console.log("Query Parameters: ", req.query);
-            result = await bedtimes.getAll(req.query);
-            console.log("Bedtimes fetched: ", result);
-        }
-        catch (error) {
-            console.error("Error fetching bedtimes: ", error);
-            response.status(500).send("Error fetching bedtimes");
-        }
-        response.json({'data': result });
+    try {
+        console.log("Query Parameters: ", req.query);
+        result = await bedtimes.getAll(req.query);
+        console.log("Bedtimes fetched: ", result);
     }
+    catch (error) {
+        console.error("Error fetching bedtimes: ", error);
+        res.status(500).send("Error fetching bedtimes");
+    }
+    res.json({'data': result });
 });
 
 app.get('/events/', async (req, res) => {
     let result = {};
-    async (request, response) => {
-        try {
-            console.log("Query Parameters: ", req.query);
-            result = await event.getAll(req.query);
-            console.log("Events fetched: ", result);
-        }
-        catch (error) {
-            console.error("Error fetching events: ", error);
-            response.status(500).send("Error fetching events");
-        }
-        response.json({'data': result });
+    try {
+        console.log("Query Parameters: ", req.query);
+        result = await event.getAll(req.query);
+        console.log("Events fetched: ", result);
     }
+    catch (error) {
+        console.error("Error fetching events: ", error);
+        res.status(500).send("Error fetching events");
+    }
+    res.json({'data': result });
 });
 
 
